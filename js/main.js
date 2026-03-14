2848638066d539614c1a0b2ea9aa2d17d99642ce
Hacesgd <162230713+Hacefr@users.noreply.github.com>
2026-03-14 11:17:57 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,111 @@
-import routes from './routes.js';
+import { round, score } from './score.js';
 
-export const store = Vue.reactive({
-    dark: JSON.parse(localStorage.getItem('dark')) || false,
-    toggleDark() {
-        this.dark = !this.dark;
-        localStorage.setItem('dark', JSON.stringify(this.dark));
-    },
-});
+/**
+ * Path to directory containing `_list.json` and all levels
+ * Removed the leading slash to make it relative to your folder
+ */
+const dir = 'data';
 
-const app = Vue.createApp({
-    data: () => ({ store }),
-});
+export async function fetchList() {
+    // Relative fetch: looks inside wertzium-demonlist/data/
+    const listResult = await fetch(`${dir}/_list.json`);
+    try {
+        const list = await listResult.json();
+        return await Promise.all(
+            list.map(async (path, rank) => {
+                const levelResult = await fetch(`${dir}/${path}.json`);
+                try {
+                    const level = await levelResult.json();
+                    return [
+                        {
+                            ...level,
+                            path,
+                            records: (level.records || []).sort(
+                                (a, b) => b.percent - a.percent,
+                            ),
+                        },
+                        null,
+                    ];
+                } catch {
+                    console.error(`Failed to load level #${rank + 1} ${path}.`);
+                    return [null, path];
+                }
+            }),
+        );
+    } catch {
+        console.error(`Failed to load list.`);
+        return null;
+    }
+}
 
-const router = VueRouter.createRouter({
-    // Standard hash history is most compatible with GitHub Pages subfolders
-    history: VueRouter.createWebHashHistory(),
-    routes,
-});
+export async function fetchEditors() {
+    try {
+        const editorsResults = await fetch(`${dir}/_editors.json`);
+        const editors = await editorsResults.json();
+        return editors;
+    } catch {
+        return null;
+    }
+}
 
-app.use(router);
-app.mount('#app');
+export async function fetchLeaderboard() {
+    const list = await fetchList();
+    if (!list) return [[], []];
+
+    const scoreMap = {};
+    const errs = [];
+    list.forEach(([level, err], rank) => {
+        if (err) {
+            errs.push(err);
+            return;
+        }
+
+        const verifier = Object.keys(scoreMap).find(
+            (u) => u.toLowerCase() === level.verifier.toLowerCase(),
+        ) || level.verifier;
+        scoreMap[verifier] ??= { verified: [], completed: [], progressed: [] };
+        
+        const { verified } = scoreMap[verifier];
+        verified.push({
+            rank: rank + 1,
+            level: level.name,
+            score: score(rank + 1, 100, level.percentToQualify),
+            link: level.verification,
+        });
+
+        (level.records || []).forEach((record) => {
+            const user = Object.keys(scoreMap).find(
+                (u) => u.toLowerCase() === record.user.toLowerCase(),
+            ) || record.user;
+            scoreMap[user] ??= { verified: [], completed: [], progressed: [] };
+            
+            const { completed, progressed } = scoreMap[user];
+            if (record.percent === 100) {
+                completed.push({
+                    rank: rank + 1,
+                    level: level.name,
+                    score: score(rank + 1, 100, level.percentToQualify),
+                    link: record.link,
+                });
+                return;
+            }
+            progressed.push({
+                rank: rank + 1,
+                level: level.name,
+                percent: record.percent,
+                score: score(rank + 1, record.percent, level.percentToQualify),
+                link: record.link,
+            });
+        });
+    });
+
+    const res = Object.entries(scoreMap).map(([user, scores]) => {
+        const { verified, completed, progressed } = scores;
+        const total = [verified, completed, progressed]
+            .flat()
+            .reduce((prev, cur) => prev + cur.score, 0);
+        return { user, total: round(total), ...scores };
+    });
+
+    return [res.sort((a, b) => b.total - a.total), errs];
+}
